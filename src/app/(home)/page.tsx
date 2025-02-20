c14d92b8b2fb808f09e2352a187a95e713a0ded8
JoaoMarqueson <marquesonj@gmail.com>
2025-02-20 19:27:03 -0300

    Config end

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,7 @@ import { Radio } from 'lucide-react'
 import Image from 'next/image'
 import logo from '../../assets/assets/logo.svg'
 import { SubscriptionForm } from './subscriptionform'
+import { Suspense } from 'react'
 
 export default function Home() {
   return (
@@ -36,7 +37,10 @@ export default function Home() {
             Dias 15 a 17 de março | Das 18h às 21h | Online & Gratuito{' '}
           </p>
         </div>
+
+        <Suspense>
         <SubscriptionForm />
+        </Suspense>  
       </div>
     </div>
   )
